e70bb8736341e7d363f99d85c08a4d58ab1ab820
akashkv2438 <akashkv11@gmail.com>
2024-06-25 11:36:53 +0530

    typing expertise

--- js/typingeff.js
+++ js/typingeff.js
@@ -1,25 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const expertiseElement = document.getElementById("expertise-typing");
-    
-//     function typeExpertise() {
-//         expertiseElement.classList.remove("fade-out");
-//         expertiseElement.style.width = '0';
-//         expertiseElement.style.animation = "none";
-        
-//         setTimeout(() => {
-//             expertiseElement.style.visibility = "visible";
-//             expertiseElement.style.animation = "typing 4s steps(20, end) infinite, blink-caret 0.75s step-end infinite";
-//         }, 20); // Short delay to reset animation
-//     }
-    
-//     // Initial call to start typing effect
-//     typeExpertise();
-    
-//     expertiseElement.addEventListener("animationiteration", function() {
-//         expertiseElement.classList.add("fade-out");
-//         setTimeout(typeExpertise, 3000);
-//     });
-// });
+
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
